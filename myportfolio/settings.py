9600ceac7b1f847b02c85235d96fa13205d633a3
Ibrahim Adel <ibrahim.zahra91@eng1.cu.edu.eg>
2018-05-13 17:22:07 +0200

    added local support

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -132,3 +132,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 #add url to access this media
 MEDIA_URL='/media/'
+try:
+    from local_settings import *
+except ImportError:
+    pass
