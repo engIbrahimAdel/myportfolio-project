c874c328bb7d93ebfd515cc977867b6a59898a6a
Ibrahim Adel <ibrahim.zahra91@eng1.cu.edu.eg>
2018-05-13 18:39:23 +0200

    change in settings

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'myportfolio',
         'USER':'myportfoliodb',
-        'PASSWORD':'hamey12345',
+        'PASSWORD':'django12345',
         'HOST':'127.0.0.1',
         'PORT':'5432',
     }
